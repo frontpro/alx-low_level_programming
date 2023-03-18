bb85f4d292a9be1131f54b8f761902fbe173ba52
frontpro <aly.arbab@gmail.com>
2023-03-18 06:30:55 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - alphabet
+*Return: 0 SUCCES
+*/
+int main(void)
+{
+int i,j;
+for (i = 97; i <= 122; i++)
+putchar(i);
+putchar('\n');
+for (j = 65; j <= 90; j++)
+putchar(j);
+putchar('\n');
+return (0);
+}
