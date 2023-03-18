71d9fbba9d4fefe76835bfe1067a82b2a7a1f98a
Alyaa <33777987+frontpro@users.noreply.github.com>
2023-03-18 17:15:51 +0200

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,12 +8,11 @@ int main(void)
 int x, e, q;
 e = 113;
 q = 101;
-for(x = 97; x <= 122; x++)
+for (x = 97; x <= 122; x++)
 {
 if (x != e && x != q)
 putchar(x);
-putchar('\n');
 }
-
+putchar('\n');
 return (0);
 }
