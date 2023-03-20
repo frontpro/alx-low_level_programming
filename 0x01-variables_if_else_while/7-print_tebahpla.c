55bb2788e1ccfb898f2aa8c602d47d3b027ab38d
frontpro <aly.arbab@gmail.com>
2023-03-19 23:24:50 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+* main -  prints the lowercase alphabet in reverse
+* Return: 0
+*/
+int main(void)
+{
+	int c;
+
+	for (c = 122; c <= 97; c--)
+	putchar(c);
+	putchar('\n');
+
+	return (0);
+}
