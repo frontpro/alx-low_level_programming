53b4e1e6c9ac0b09c1dee236fd81731724b936bf
frontpro <aly.arbab@gmail.com>
2023-03-21 09:22:20 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - a function that prints 10 times the alphabt
+* Return: 0
+*/
+void print_alphabet_x10(void)
+{
+	int i, c;
+
+	for (i = 0; i < 10; i++)
+		for (c = 97; c <= 122; c++)
+		_putchar(c);
+		_putchar('\n');
+}
