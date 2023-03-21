50b127d281fceb603fa0772897e02398be14dfac
frontpro <aly.arbab@gmail.com>
2023-03-21 09:00:32 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* main - a function that prints the alphabet, in lowercase.
+* Return: 0
+*/
+void print_alphabet(void)
+{
+	int c;
+
+	for (c = 97; c <= 122; c++)
+	_putchar(c);
+	_putchar('\n');
+}
