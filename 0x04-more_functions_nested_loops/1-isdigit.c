981e6eb61181ace22437be811e0f9650e868d376
frontpro <aly.arbab@gmail.com>
2023-03-24 15:34:25 -0700

    more nested loops

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* _isdigit - a function that checks for a digit
+* Return: true or false
+* @c: char param
+*/
+int _isdigit(int c)
+{
+return (c >= 48 && c <= 57);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN
+#define MAIN
+void print_number(int n);
+void print_triangle(int size);
+void print_square(int size);
+void print_diagonal(int n);
+void print_line(int n);
+void more_numbers(void);
+void print_most_numbers(void);
+int _putchar(char c);
+void print_numbers(void);
+int mul(int a, int b);
+int _isupper(int c);
+int _isdigit(int c);
+#endif
