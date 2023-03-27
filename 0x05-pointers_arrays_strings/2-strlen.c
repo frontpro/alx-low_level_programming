9e762ba8a3f18d397794ec6e730d89196d872880
frontpro <aly.arbab@gmail.com>
2023-03-27 08:16:08 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+* _strlen -  a function that returns the length of a string
+* @i: input
+* @s: array
+* Return: value of i
+*/
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; *s != '\0'; ++i)
+	return (i);
+}
