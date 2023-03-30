b54b278e871ed8984956b822e04b21654a600c36
frontpro <aly.arbab@gmail.com>
2023-03-30 06:29:15 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,12 +9,12 @@ char *_strcat(char *dest, char *src)
 	int i = 0;
 	int j;
 
-	for(i = 0; dest[i] != '\0'; i++);
-	for(j = 0; src[j] != '\0'; j++)
+	for (i = 0; dest[i] != '\0'; i++);
+	for (j = 0; src[j] != '\0'; j++)
 	{
 		dest[i] = src[j];
 		i++;
 	}
-	dest[i]='\0';
+	dest[i] = '\0';
 	return (dest);
 }
