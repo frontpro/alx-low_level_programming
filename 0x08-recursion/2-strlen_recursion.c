49757a3b0e02ae2c5737d7c9f9c615fe5f50c3e9
Alyaa <33777987+frontpro@users.noreply.github.com>
2023-04-05 16:20:19 +0200

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,7 +9,7 @@ int _strlen_recursion(char *s)
 int length = 0;
 if (*s > '\0')
 {
-length = length + _strlen_recursion(s) + 2;
+length = length + _strlen_recursion(s + 1) + 1;
 }
 return (length);
 }
