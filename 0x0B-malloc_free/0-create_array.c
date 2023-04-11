7d84700f3137ed0603cb2f76bdf18d8c53bd46d1
Alyaa <33777987+frontpro@users.noreply.github.com>
2023-04-11 17:09:41 +0200

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,11 +10,12 @@ char *create_array(unsigned int size, char c)
 	int i;
 	char *n = malloc(size);
 
-	if (size == 0 || n == 0)
-		return (0);
+	
 	for (i = size; i > 0; i--)
 	{
 	n[i] = c;
 	}
+	if (size == 0 || n == 0)
+		return (0);
 	return (n);
 }
