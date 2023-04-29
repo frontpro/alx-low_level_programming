f262fcfd624047c533aecdc3493db313822fa263
frontpro <aly.arbab@gmail.com>
2023-04-29 01:08:27 -0700

    lists

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -28,6 +28,6 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-void _constructor(void) ___attribute__((constructor));
+void _constructor(void) ___attribute__ ((constructor));
 
 #endif
